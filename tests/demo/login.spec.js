45ca688ee299f66680bafacc6ae83c29f667fa6c
Developer <developer@example.com>
2026-02-23 01:09:34 +0530

    Added spacing for which code is POM and Manual

--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -4,10 +4,10 @@ import { LoginPage } from '../../pages/login'
 test('test', async ({ page }) => {
 
   const Login = new LoginPage(page)
-
+console.log(".......................... from the POM test form page login.js class ..........................")
   await Login.gotoLoginPage()
   await Login.login('tomsmith', 'SuperSecretPassword!')
-
+console.log(".......................... from the hardcoded test form page login.js class ..........................")
   await page.goto('https://the-internet.herokuapp.com/login');
   await page.getByLabel('Username').click();
    await page.getByLabel('Username').fill('tomsmith');
