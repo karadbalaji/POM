90bb39cb5e905b44997a9933e069a1d1c4dfa387
Developer <developer@example.com>
2026-02-23 02:00:45 +0530

    i chnages the login class with login button comment

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ exports.LoginPage = class LoginPage {
         this.page = page
         this.username_textbox = page.getByLabel('Username')  //Locater is still good no need to update
         this.password_textbox = page.getByLabel('Password')
-        this.login_button = page.getByRole('button', { name: 'Login' })
+        this.login_button = page.getByRole('button', { name: 'Login' }) //no need to update the locator as the name is same but the icon is added in the button which is not effecting the locator
     }
 
     async gotoLoginPage(){
